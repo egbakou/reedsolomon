5eb8a49eb149c945c6f61b034f45ec5ae3e8e72d
Laurent Egbakou <26142591+egbakou@users.noreply.github.com>
2022-08-22 00:06:37 +0200

    test: added matrix test

---
+++ tests/ReedSolomon.NET.Tests/MatrixTests.cs
@@ -0,0 +1,48 @@
+﻿// Matrix Tests
+// Copyright © 2022 Kodjo Laurent Egbakou
+// Copyright 2015, Backblaze, Inc.  All rights reserved.
+
+namespace ReedSolomon.NET.Tests;
+
+public class MatrixTests
+{
+    [Fact]
+    public void Identity_Should_Return_The_Right_Matrix() =>
+        Matrix.Identity(3).ToString().ShouldBe("[1, 0, 0]\n[0, 1, 0]\n[0, 0, 1]\n");
+
+    [Fact]
+    public void ToOneLineString_Should_Return_The_Right_String() =>
+        Matrix.Identity(3).ToOneLineString().ShouldBe("[[1, 0, 0], [0, 1, 0], [0, 0, 1]]");
+
+    [Fact]
+    public void Multiply_Two_Matrices_Should_Return_The_Right_Matrix()
+    {
+        var m1 = new Matrix(new[]
+        {
+            new byte []{ 1, 2 },
+            new byte []{ 3, 4 }
+        });
+
+        var m2 = new Matrix(new[]
+        {
+            new byte []{ 5, 6 },
+            new byte []{ 7, 8 }
+        });
+        var result = m1.Multiply(m2);
+        result.ToString().ShouldBe("[11, 22]\n[19, 42]\n");
+    }
+
+    [Fact]
+    public void Inverse_Matrix_Should_Return_The_Right_Matrix()
+    {
+        Matrix m = new (
+            new[]
+            {
+                new byte [] { 56, 23, 98 },
+                new byte [] { 3, 100, 200 },
+                new byte [] { 45, 201, 123 }
+            });
+        m.Invert().ToString().ShouldBe("[175, 133, 33]\n[130, 13, 245]\n[112, 35, 126]\n");
+        Matrix.Identity(3).ShouldBe(m.Multiply(m.Invert()));
+    }
+}

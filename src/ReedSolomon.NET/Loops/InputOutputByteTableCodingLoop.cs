9c8f7a2e110ad67d434ece85666e713190bf2c75
Laurent Egbakou <26142591+egbakou@users.noreply.github.com>
2022-08-22 00:04:12 +0200

    feat: added InputOutputByteTableCodingLoop

---
+++ src/ReedSolomon.NET/Loops/InputOutputByteTableCodingLoop.cs
@@ -0,0 +1,96 @@
+﻿// One specific ordering/nesting of the coding loops.
+// Copyright © 2022 Kodjo Laurent Egbakou
+// Copyright 2015, Backblaze, Inc.  All rights reserved.
+
+namespace ReedSolomon.NET.Loops
+{
+    /// <summary>
+    /// One specific ordering/nesting of the coding loops.
+    /// </summary>
+    public class InputOutputByteTableCodingLoop : CodingLoopBase
+    {
+        /// <inheritdoc />
+        public override void CodeSomeShards(byte[][] matrixRows, byte[][] inputs,
+            in int inputCount, byte[][] outputs,
+            in int outputCount, in int offset, in int byteCount)
+        {
+            var table = Galois.MultiplicationTable;
+
+            const int iInputStart = 0;
+            var inputShardStart = inputs[iInputStart];
+            for (var iOutput = 0; iOutput < outputCount; iOutput++)
+            {
+                var outputShard = outputs[iOutput];
+                var matrixRow = matrixRows[iOutput];
+                var multTableRow = table[matrixRow[iInputStart] & 0xFF];
+                for (var iByte = offset; iByte < offset + byteCount; iByte++)
+                {
+                    outputShard[iByte] = multTableRow[inputShardStart[iByte] & 0xFF];
+                }
+            }
+
+            for (var iInput = 1; iInput < inputCount; iInput++)
+            {
+                var inputShard = inputs[iInput];
+                for (var iOutput = 0; iOutput < outputCount; iOutput++)
+                {
+                    var outputShard = outputs[iOutput];
+                    var matrixRow = matrixRows[iOutput];
+                    var multTableRow = table[matrixRow[iInput] & 0xFF];
+                    for (var iByte = offset; iByte < offset + byteCount; iByte++)
+                    {
+                        outputShard[iByte] ^= multTableRow[inputShard[iByte] & 0xFF];
+                    }
+                }
+            }
+        }
+
+        /// <inheritdoc />
+        public override bool CheckSomeShards(byte[][] matrixRows, byte[][] inputs,
+            in int inputCount, byte[][] toCheck,
+            in int checkCount, in int offset, in int byteCount, in byte[]? tempBuffer)
+        {
+            if (tempBuffer is null)
+            {
+                return base.CheckSomeShards(matrixRows, inputs, inputCount, toCheck, checkCount, offset, byteCount,
+                    null);
+            }
+
+            var table = Galois.MultiplicationTable;
+
+            for (var iOutput = 0; iOutput < checkCount; iOutput++)
+            {
+                var outputShard = toCheck[iOutput];
+                var matrixRow = matrixRows[iOutput];
+
+                const int iInputStart = 0;
+                var inputShardStart = inputs[iInputStart];
+                var multTableRowStart = table[matrixRow[iInputStart] & 0xFF];
+                for (var iByte = offset; iByte < offset + byteCount; iByte++)
+                {
+                    tempBuffer[iByte] = multTableRowStart[inputShardStart[iByte] & 0xFF];
+                }
+
+                for (var iInput = 1; iInput < inputCount; iInput++)
+                {
+                    var inputShard = inputs[iInput];
+                    var multTableRow = table[matrixRow[iInput] & 0xFF];
+                    for (var iByte = offset; iByte < offset + byteCount; iByte++)
+                    {
+                        tempBuffer[iByte] ^= multTableRow[inputShard[iByte] & 0xFF];
+                    }
+                }
+
+                for (var iByte = offset; iByte < offset + byteCount; iByte++)
+                {
+                    if (tempBuffer[iByte] != outputShard[iByte])
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+}
